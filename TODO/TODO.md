ddedcedf6fd758032c99941cc23fa0da3055250c
Henri <zhtyz12@gmail.com>
2026-02-05 00:20:30 -0300

    henri: verificador de argumentos(provavelmente lixo)

---
+++ TODO/TODO.md
@@ -0,0 +1,6 @@
+05/02/2026
+TODO Por: Henri
+Arquivo referido: 'scanners/dir_scanner.rs'
+
+## para fazer:
+- otimizar o verificador de parâmetros
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+//=================================================================================================
+//arquivo para "satisfazer" o cargo
+//ou seja nao remova o main.rs pq se nao vc nao vai poder utilizar cargo build para atualizar as
+//dependencias
+//==============================================================================================
+fn main() {}
