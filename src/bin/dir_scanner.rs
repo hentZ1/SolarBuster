508ba59e1f43db388d4b135bb0f92dd89d06a426
henri <zhtyz12@gmail.com>
2026-02-24 13:18:38 -0300

    zhtyz: mudanças menores sobre coisa da wordlist
    oq foi mudado: adicionado mais um parametro sendo ele 'concurrency' ou seja agora os workers nao sao fixo e o user pode escolher a sua quantidade, tambem foi adicionado a msg de help para os parametros, tambem foi mudado alguns 'get' por 'head' e agora comparar o noise com a url sendo escaneada é mais rapido

--- src/bin/dir_scanner.rs
+++ src/bin/dir_scanner.rs
@@ -15,25 +15,41 @@ use tokio::{
     about = "Fast web directory enumerator in Rust"
 )]
 struct Args {
-    #[arg(short = 'u', long = "url")]
+    #[arg(
+        short = 'u',
+        long = "url",
+        help = "the url of the website you want to enumerate"
+    )]
     url: String,
 
-    #[arg(short = 'w', long = "wordlist")]
+    #[arg(
+        short = 'w',
+        long = "wordlist",
+        help = "your word list for the enumerator"
+    )]
     path: String,
+
+    #[arg(
+        short = 'c',
+        default_value_t = 50,
+        long = "concurrency",
+        help = "number of competing workers"
+    )]
+    workers: usize,
 }
 
 const QUEUE_SIZE: usize = 1000;
-const WORKERS: usize = 50;
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
     let url = args.url;
     let path = args.path;
+    let workers = args.workers;
 
     let (tx, mut rx) = mpsc::channel::<String>(QUEUE_SIZE);
 
-    let sem = Arc::new(Semaphore::new(WORKERS));
+    let sem = Arc::new(Semaphore::new(workers));
 
     tokio::spawn(reader(path, tx));
 
@@ -65,8 +81,12 @@ async fn measure_noise(url: String, client: Client) -> Option<u64> {
 }
 
 async fn reader(path: String, tx: mpsc::Sender<String>) {
-    let file = File::open(path).await.unwrap();
+    let file = File::open(path)
+        .await
+        .expect("it was not possible to read or find the file ");
+
     let reader = BufReader::new(file);
+
     let mut lines = reader.lines();
 
     while let Some(line) = lines.next_line().await.unwrap() {
@@ -86,7 +106,7 @@ async fn worker(client: Client, url: String, word: String) {
 
     let noise = measure_noise(url, client.clone()).await.unwrap_or(0);
 
-    if let Ok(resp) = client.get(&full_url).send().await
+    if let Ok(resp) = client.head(&full_url).send().await
         && resp.status().is_success()
         && let Some(len) = resp.content_length()
         && len != noise
